a57d3de3b6c4f253a3d9b36d63e0633f2335acc1
ghostbuster91 <ghostbuster91@users.noreply.github.com>
2023-07-01 08:33:20 +0200

    Bug in nixos bridgeVLANs

--- nixos/network.nix
+++ nixos/network.nix
@@ -122,24 +122,37 @@ in
         networkConfig = {
           ConfigureWithoutCarrier = true;
         };
-        linkConfig.RequiredForOnline = "configured";
+        linkConfig.RequiredForOnline = "routable";
         address = [
-          "192.168.10.100/24"
+          "192.168.10.1/24"
         ];
       };
       # # Configure the bridge for its desired function
       "40-br0" = {
-        matchConfig.Name = "br";
+        matchConfig.Name = "br0";
         bridgeConfig = { };
         linkConfig = {
           # or "routable" with IP addresses configured
-          # RequiredForOnline = "carrier";
+          RequiredForOnline = "routable";
         };
-        address = [
-          "192.168.10.1/24"
+        networkConfig = {
+            LinkLocalAddressing = "no";
+        };
+        bridgeVLANs=[
+        {bridgeVLANConfig={
+          VLAN=120;
+          PVID=120;
+          EgressUntagged=120;
+        };}];
+        vlan = [
+          "vlan120"
         ];
-        networkConfig = { };
-
+      };
+      "45-vlan120" = {
+        matchConfig.Name = "vlan120";
+        address = [
+          "192.168.15.1/24"
+        ];
       };
       "10-wan" = {
         matchConfig.Name = "wan";
@@ -160,140 +173,140 @@ in
   };
 
   # wireless access point
-  services.hostapd = {
-    enable = true;
-    radios = {
-      wlan0 = {
-        hwMode = "g";
-        countryCode = "PL";
-        channel = 0; # ACS
-
-        # use 'iw phy#1 info' to determine your VHT capabilities
-        wifi4 = {
-          enable = true;
-          capabilities = [ "HT40+" "LDPC" "SHORT-GI-20" "SHORT-GI-40" "TX-STBC" "RX-STBC1" "MAX-AMSDU-7935" ];
-        };
-        networks = {
-          wlan0 = {
-            ssid = "koteczkowo5";
-            authentication = {
-              mode = "wpa3-sae";
-              saePasswordsFile = config.sops.secrets.wifiPassword.path;
-            };
-            bssid = "36:b9:02:21:08:00";
-            settings = {
-              bridge = "br0";
-            };
-          };
-          # Uncomment when needed otherwise remove
-          wlan0-1 = {
-            ssid = "koteczkowo3";
-            authentication = {
-              mode = "none"; # this is overriden by settings
-            };
-            managementFrameProtection = "optional";
-            bssid = "e6:02:43:07:00:00";
-            settings = {
-              bridge = "br0";
-              wpa = lib.mkForce 2;
-              wpa_key_mgmt = "WPA-PSK";
-              wpa_pairwise = "CCMP";
-              wpa_passphrase = config.sops.secrets.legacyWifiPassword.path;
-            };
-          };
-        };
-      };
-      wlan1 = {
-        hwMode = "a";
-        # channels with 160 MHz width in Poland: 36, 52, 100 i 116
-        channel = 0; # ACS
-        countryCode = "PL";
-
-        # use 'iw phy#1 info' to determine your VHT capabilities
-        wifi4 = {
-          enable = true;
-          capabilities = [ "HT40+" "LDPC" "SHORT-GI-20" "SHORT-GI-40" "TX-STBC" "RX-STBC1" "MAX-AMSDU-7935" ];
-        };
-        wifi5 = {
-          enable = true;
-          operatingChannelWidth = "160";
-          capabilities = [ "RXLDPC" "SHORT-GI-80" "SHORT-GI-160" "TX-STBC-2BY1" "SU-BEAMFORMER" "SU-BEAMFORMEE" "MU-BEAMFORMER" "MU-BEAMFORMEE" "RX-ANTENNA-PATTERN" "TX-ANTENNA-PATTERN" "RX-STBC-1" "SOUNDING-DIMENSION-4" "BF-ANTENNA-4" "VHT160" "MAX-MPDU-11454" "MAX-A-MPDU-LEN-EXP7" ];
-        };
-        wifi6 = {
-          enable = true;
-          singleUserBeamformer = true;
-          singleUserBeamformee = true;
-          multiUserBeamformer = true;
-          operatingChannelWidth = "160";
-        };
-        settings = {
-          # these two are mandatory for wifi 5 & 6 to work
-          vht_oper_centr_freq_seg0_idx = 50;
-          he_oper_centr_freq_seg0_idx = 50;
-
-          # The "tx_queue_data2_burst" parameter in Linux refers to the burst size for 
-          # transmitting data packets from the second data queue of a network interface. 
-          # It determines the number of packets that can be sent in a burst. 
-          # Adjusting this parameter can impact network throughput and latency.
-          tx_queue_data2_burst = 2;
-
-
-          # The "he_bss_color" parameter in Wi-Fi 6 (802.11ax) refers to the BSS Color field in the HE (High Efficiency) MAC header.
-          # BSS Color is a mechanism introduced in Wi-Fi 6 to mitigate interference and improve network efficiency in dense deployment scenarios. 
-          # It allows multiple overlapping Basic Service Sets (BSS) to differentiate and coexist in the same area without causing excessive interference.
-          he_bss_color = 63; # was set to 128 by openwrt but range of possible values in 2.10 is 1-63
-
-          # Magic values that were set by openwrt but I didn't bother inspecting every single one
-          he_spr_sr_control = 3;
-          he_default_pe_duration = 4;
-          he_rts_threshold = 1023;
-
-          he_mu_edca_qos_info_param_count = 0;
-          he_mu_edca_qos_info_q_ack = 0;
-          he_mu_edca_qos_info_queue_request = 0;
-          he_mu_edca_qos_info_txop_request = 0;
-
-          # he_mu_edca_ac_be_aci=0; missing in 2.10
-          he_mu_edca_ac_be_aifsn = 8;
-          he_mu_edca_ac_be_ecwmin = 9;
-          he_mu_edca_ac_be_ecwmax = 10;
-          he_mu_edca_ac_be_timer = 255;
-
-          he_mu_edca_ac_bk_aifsn = 15;
-          he_mu_edca_ac_bk_aci = 1;
-          he_mu_edca_ac_bk_ecwmin = 9;
-          he_mu_edca_ac_bk_ecwmax = 10;
-          he_mu_edca_ac_bk_timer = 255;
-
-          he_mu_edca_ac_vi_ecwmin = 5;
-          he_mu_edca_ac_vi_ecwmax = 7;
-          he_mu_edca_ac_vi_aifsn = 5;
-          he_mu_edca_ac_vi_aci = 2;
-          he_mu_edca_ac_vi_timer = 255;
-
-          he_mu_edca_ac_vo_aifsn = 5;
-          he_mu_edca_ac_vo_aci = 3;
-          he_mu_edca_ac_vo_ecwmin = 5;
-          he_mu_edca_ac_vo_ecwmax = 7;
-          he_mu_edca_ac_vo_timer = 255;
-        };
-        networks = {
-          wlan1 = {
-            ssid = "koteczkowo5";
-            authentication = {
-
-              mode = "wpa3-sae";
-              saePasswordsFile = config.sops.secrets.wifiPassword.path; # Use saePasswordsFile if possible.
-            };
-            bssid = "36:b9:02:21:08:a2";
-            settings = {
-              bridge = "br0";
-            };
-          };
-        };
-      };
-    };
-  };
+  # services.hostapd = {
+  #   enable = true;
+  #   radios = {
+  #     wlan0 = {
+  #       hwMode = "g";
+  #       countryCode = "PL";
+  #       channel = 0; # ACS
+  #
+  #       # use 'iw phy#1 info' to determine your VHT capabilities
+  #       wifi4 = {
+  #         enable = true;
+  #         capabilities = [ "HT40+" "LDPC" "SHORT-GI-20" "SHORT-GI-40" "TX-STBC" "RX-STBC1" "MAX-AMSDU-7935" ];
+  #       };
+  #       networks = {
+  #         wlan0 = {
+  #           ssid = "koteczkowo5";
+  #           authentication = {
+  #             mode = "wpa3-sae";
+  #             saePasswordsFile = config.sops.secrets.wifiPassword.path;
+  #           };
+  #           bssid = "36:b9:02:21:08:00";
+  #           settings = {
+  #             bridge = "br0";
+  #           };
+  #         };
+  #         # Uncomment when needed otherwise remove
+  #         wlan0-1 = {
+  #           ssid = "koteczkowo3";
+  #           authentication = {
+  #             mode = "none"; # this is overriden by settings
+  #           };
+  #           managementFrameProtection = "optional";
+  #           bssid = "e6:02:43:07:00:00";
+  #           settings = {
+  #             bridge = "br0";
+  #             wpa = lib.mkForce 2;
+  #             wpa_key_mgmt = "WPA-PSK";
+  #             wpa_pairwise = "CCMP";
+  #             wpa_passphrase = config.sops.secrets.legacyWifiPassword.path;
+  #           };
+  #         };
+  #       };
+  #     };
+  #     wlan1 = {
+  #       hwMode = "a";
+  #       # channels with 160 MHz width in Poland: 36, 52, 100 i 116
+  #       channel = 0; # ACS
+  #       countryCode = "PL";
+  #
+  #       # use 'iw phy#1 info' to determine your VHT capabilities
+  #       wifi4 = {
+  #         enable = true;
+  #         capabilities = [ "HT40+" "LDPC" "SHORT-GI-20" "SHORT-GI-40" "TX-STBC" "RX-STBC1" "MAX-AMSDU-7935" ];
+  #       };
+  #       wifi5 = {
+  #         enable = true;
+  #         operatingChannelWidth = "160";
+  #         capabilities = [ "RXLDPC" "SHORT-GI-80" "SHORT-GI-160" "TX-STBC-2BY1" "SU-BEAMFORMER" "SU-BEAMFORMEE" "MU-BEAMFORMER" "MU-BEAMFORMEE" "RX-ANTENNA-PATTERN" "TX-ANTENNA-PATTERN" "RX-STBC-1" "SOUNDING-DIMENSION-4" "BF-ANTENNA-4" "VHT160" "MAX-MPDU-11454" "MAX-A-MPDU-LEN-EXP7" ];
+  #       };
+  #       wifi6 = {
+  #         enable = true;
+  #         singleUserBeamformer = true;
+  #         singleUserBeamformee = true;
+  #         multiUserBeamformer = true;
+  #         operatingChannelWidth = "160";
+  #       };
+  #       settings = {
+  #         # these two are mandatory for wifi 5 & 6 to work
+  #         vht_oper_centr_freq_seg0_idx = 50;
+  #         he_oper_centr_freq_seg0_idx = 50;
+  #
+  #         # The "tx_queue_data2_burst" parameter in Linux refers to the burst size for 
+  #         # transmitting data packets from the second data queue of a network interface. 
+  #         # It determines the number of packets that can be sent in a burst. 
+  #         # Adjusting this parameter can impact network throughput and latency.
+  #         tx_queue_data2_burst = 2;
+  #
+  #
+  #         # The "he_bss_color" parameter in Wi-Fi 6 (802.11ax) refers to the BSS Color field in the HE (High Efficiency) MAC header.
+  #         # BSS Color is a mechanism introduced in Wi-Fi 6 to mitigate interference and improve network efficiency in dense deployment scenarios. 
+  #         # It allows multiple overlapping Basic Service Sets (BSS) to differentiate and coexist in the same area without causing excessive interference.
+  #         he_bss_color = 63; # was set to 128 by openwrt but range of possible values in 2.10 is 1-63
+  #
+  #         # Magic values that were set by openwrt but I didn't bother inspecting every single one
+  #         he_spr_sr_control = 3;
+  #         he_default_pe_duration = 4;
+  #         he_rts_threshold = 1023;
+  #
+  #         he_mu_edca_qos_info_param_count = 0;
+  #         he_mu_edca_qos_info_q_ack = 0;
+  #         he_mu_edca_qos_info_queue_request = 0;
+  #         he_mu_edca_qos_info_txop_request = 0;
+  #
+  #         # he_mu_edca_ac_be_aci=0; missing in 2.10
+  #         he_mu_edca_ac_be_aifsn = 8;
+  #         he_mu_edca_ac_be_ecwmin = 9;
+  #         he_mu_edca_ac_be_ecwmax = 10;
+  #         he_mu_edca_ac_be_timer = 255;
+  #
+  #         he_mu_edca_ac_bk_aifsn = 15;
+  #         he_mu_edca_ac_bk_aci = 1;
+  #         he_mu_edca_ac_bk_ecwmin = 9;
+  #         he_mu_edca_ac_bk_ecwmax = 10;
+  #         he_mu_edca_ac_bk_timer = 255;
+  #
+  #         he_mu_edca_ac_vi_ecwmin = 5;
+  #         he_mu_edca_ac_vi_ecwmax = 7;
+  #         he_mu_edca_ac_vi_aifsn = 5;
+  #         he_mu_edca_ac_vi_aci = 2;
+  #         he_mu_edca_ac_vi_timer = 255;
+  #
+  #         he_mu_edca_ac_vo_aifsn = 5;
+  #         he_mu_edca_ac_vo_aci = 3;
+  #         he_mu_edca_ac_vo_ecwmin = 5;
+  #         he_mu_edca_ac_vo_ecwmax = 7;
+  #         he_mu_edca_ac_vo_timer = 255;
+  #       };
+  #       networks = {
+  #         wlan1 = {
+  #           ssid = "koteczkowo5";
+  #           authentication = {
+  #
+  #             mode = "wpa3-sae";
+  #             saePasswordsFile = config.sops.secrets.wifiPassword.path; # Use saePasswordsFile if possible.
+  #           };
+  #           bssid = "36:b9:02:21:08:a2";
+  #           settings = {
+  #             bridge = "br0";
+  #           };
+  #         };
+  #       };
+  #     };
+  #   };
+  # };
 
   services.resolved.enable = false;
 
@@ -307,9 +320,11 @@ in
       bogus-priv = true;
       no-resolv = true;
 
-      dhcp-range = [ "192.168.10.100,192.168.10.254" ];
-      interface = [ "br0" "lan3" ];
-      dhcp-host = "192.168.10.1";
+      dhcp-range = [ "lan3,192.168.10.100,192.168.10.254" 
+"vlan120,192.168.15.100,192.168.15.254"
+      ];
+      interface = [ "vlan120" "lan3" ];
+      dhcp-host = ["vlan120,192.168.15.1" "lan3,192.168.10.1"];
 
       # local domains
       local = "/lan/";
