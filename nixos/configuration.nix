a08c60f25b186b32b3a9173274ad69aa3edd3a3f
ghostbuster91 <ghostbuster91@users.noreply.github.com>
2023-07-02 15:24:00 +0200

    Setup initialHashedPassword

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -25,6 +25,7 @@
     extraGroups = [ "wheel" "network" ]; # Enable ‘sudo’ for the user.
     shell = pkgs.zsh;
     openssh.authorizedKeys.keys = [ "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFFeU4GXH+Ae00DipGGJN7uSqPJxWFmgRo9B+xjV3mK4" ];
+    initialHashedPassword = "$y$j9T$aeZHaSe8QKeC0ruAi9TKo.$zooI/IZUwOupVDbMReaukiargPrF93H/wdR/.0zsrr.";
   };
 
   system.autoUpgrade =
