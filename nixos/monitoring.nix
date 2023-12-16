c8e9cba7daca77d6007069a480142179f645b07c
ghostbuster91 <ghostbuster91@users.noreply.github.com>
2023-12-16 18:06:24 +0100

    [Prometheus] Define list of collectors manually

--- nixos/monitoring.nix
+++ nixos/monitoring.nix
@@ -4,9 +4,58 @@
       node = {
         enable = true;
         # TODO test perf impact of these modules
-        # enabledCollectors = [ "systemd"  "wifi" "ethtool" ];
+        enabledCollectors = [
+          "arp"
+          "hwmon"
+          "cpu"
+          "diskstats"
+          "ethtool"
+          "interrupts"
+          "ksmd"
+          "lnstat"
+          "mountstats"
+          "processes"
+          "systemd"
+          "wifi"
+          "tcpstat"
+          "netdev"
+          "netstat"
+          "network_route"
+          "netclass"
+          "sockstat"
+          "stat"
+          "conntrack"
+        ];
         port = 9002;
       };
+    };
+  };
+    services.promtail = {
+    enable = true;
+    configuration = {
+      server = {
+        disable=true;
+      };
+      # positions = {
+      #   filename = "/var/log/positions.yaml";
+      # };
+      clients = [{
+        url = "http://loki.deckard.lan/loki/api/v1/push";
+      }];
+      scrape_configs = [{
+        job_name = "journal";
+        journal = {
+          max_age = "12h";
+          labels = {
+            job = "systemd-journal";
+            host = "surfer";
+          };
+        };
+        relabel_configs = [{
+          source_labels = [ "__journal__systemd_unit" ];
+          target_label = "unit";
+        }];
+      }];
     };
   };
 }
