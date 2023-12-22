1a7ec23430ffd61f1c7b53b0d4d4857c70695d90
ghostbuster91 <ghostbuster91@users.noreply.github.com>
2023-12-22 10:32:08 +0100

    Setup remote builds via nixbuild

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -133,4 +133,30 @@
 
   # replace default editor with neovim
   environment.variables.EDITOR = "nvim";
+  programs.ssh.extraConfig = ''
+    Host eu.nixbuild.net
+      PubkeyAcceptedKeyTypes ssh-ed25519
+      ServerAliveInterval 60
+      IPQoS throughput
+      IdentityFile /home/kghost/.ssh/my-nixbuild-key
+  '';
+
+  programs.ssh.knownHosts = {
+    nixbuild = {
+      hostNames = [ "eu.nixbuild.net" ];
+      publicKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPIQCZc54poJ8vqawd8TraNryQeJnvH1eLpIDgbiqymM";
+    };
+  };
+
+  nix = {
+    distributedBuilds = true;
+    buildMachines = [
+      {
+        hostName = "eu.nixbuild.net";
+        system = "aarch64-linux";
+        maxJobs = 100;
+      }
+    ];
+  };
+
 }
