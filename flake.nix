7c9d27a9270f89e4efaceb491e2ef42a393b0b04
ghostbuster91 <ghostbuster91@users.noreply.github.com>
2024-06-23 02:04:57 +0200

    Make hm follow nixpkgs

--- flake.nix
+++ flake.nix
@@ -1,7 +1,10 @@
 {
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
-    home-manager.url = "github:nix-community/home-manager/release-24.05";
+    home-manager = {
+      url = "github:nix-community/home-manager/release-24.05";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
     nixos-sbc = {
       url = "github:nakato/nixos-sbc/main";
       inputs.nixpkgs.follows = "nixpkgs";
