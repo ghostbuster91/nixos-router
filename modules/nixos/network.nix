20e67dc16b823bb1eabb83718906f01d68801074
ghostbuster91 <ghostbuster91@users.noreply.github.com>
2025-01-07 21:42:11 +0100

    Repurpose to a switch

--- modules/nixos/network.nix
+++ modules/nixos/network.nix
@@ -5,7 +5,6 @@
         "net.ipv4.conf.all.forwarding" = true;
         "net.ipv6.conf.all.forwarding" = true;
         "net.ipv4.conf.br-lan.rp_filter" = 1;
-        "net.ipv4.conf.wan.rp_filter" = 1;
       };
     };
   };
@@ -19,41 +18,9 @@
     firewall.enable = false;
 
     nftables = {
-      enable = true;
+      enable = false;
       checkRuleset = false;
-      ruleset = ''
-        table inet filter {
-           flowtable f {
-             hook ingress priority 0; 
-             devices = { "wan", "lan0", "lan1", "lan2", "lan3" };
-             flags offload;
-           }
-
-          chain input {
-            type filter hook input priority 0; policy drop;
-
-            iifname { "br-lan" } accept comment "Allow local network to access the router"
-            iifname "wan" ct state { established, related } accept comment "Allow established traffic"
-            iifname "wan" icmp type { echo-request, destination-unreachable, time-exceeded } counter accept comment "Allow select ICMP"
-            iifname "wan" counter drop comment "Drop all other unsolicited traffic from wan"
-            iifname "lo" accept comment "Accept everything from loopback interface"
-          }
-          chain forward {
-            type filter hook forward priority filter; policy drop;
-            ip protocol { tcp, udp } ct state { established } flow offload @f comment "Offload tcp/udp established traffic"
-
-            iifname { "br-lan" } oifname { "wan" } accept comment "Allow trusted LAN to WAN"
-            iifname { "wan" } oifname { "br-lan" } ct state { established, related } accept comment "Allow established back to LANs"
-          }
-        }
-        
-        table ip nat {
-          chain postrouting {
-            type nat hook postrouting priority 100; policy accept;
-            oifname "wan" masquerade
-          } 
-        }
-      '';
+      ruleset = '''';
     };
   };
 
@@ -106,11 +73,17 @@
       "40-br-lan" = {
         matchConfig.Name = "br-lan";
         bridgeConfig = { };
-        address = [
-          "192.168.10.1/24"
-        ];
         networkConfig = {
+          MulticastDNS = true;
           ConfigureWithoutCarrier = true;
+          # start a DHCP Client for IPv4 Addressing/Routing
+          DHCP = "ipv4";
+          # accept Router Advertisements for Stateless IPv6 Autoconfiguraton (SLAAC)
+          IPv6AcceptRA = true;
+          DNSOverTLS = false;
+          DNSSEC = false;
+          IPv6PrivacyExtensions = false;
+          IPForward = true;
         };
         # Don't wait for it as it also would wait for wlan and DFS which takes around 5 min 
         linkConfig.RequiredForOnline = "no";
@@ -118,63 +91,18 @@
       "10-wan" = {
         matchConfig.Name = "wan";
         networkConfig = {
-          # start a DHCP Client for IPv4 Addressing/Routing
-          DHCP = "ipv4";
-          # accept Router Advertisements for Stateless IPv6 Autoconfiguraton (SLAAC)
-          IPv6AcceptRA = true;
-          DNSOverTLS = true;
-          DNSSEC = true;
-          IPv6PrivacyExtensions = false;
-          IPForward = true;
+          Bridge = "br-lan";
+          ConfigureWithoutCarrier = true;
         };
-        # make routing on this interface a dependency for network-online.target
-        linkConfig.RequiredForOnline = "routable";
+        linkConfig.RequiredForOnline = "enslaved";
       };
     };
   };
-  services.resolved.enable = false;
-
-  services.dnsmasq = {
+  services.resolved = {
     enable = true;
-    settings = {
-      # upstream DNS servers
-      server = [ "9.9.9.9" "8.8.8.8" "1.1.1.1" ];
-      # sensible behaviours
-      domain-needed = true;
-      bogus-priv = true;
-      no-resolv = true;
-
-      # Cache dns queries.
-      cache-size = 1000;
-
-      dhcp-range = [ "br-lan,192.168.10.50,192.168.10.254,24h" ];
-      interface = "br-lan";
-      dhcp-host = "192.168.10.1";
-
-      # local domains
-      local = "/lan/";
-      domain = "lan";
-      expand-hosts = true;
-
-      # don't use /etc/hosts as this would advertise surfer as localhost
-      no-hosts = true;
-      address = [
-        "/surfer.lan/192.168.10.1"
-        "/.deckard.lan/192.168.10.113"
-      ];
-    };
-  };
-
-  # mDNS
-  services = {
-    avahi = {
-      enable = true;
-      nssmdns4 = true;
-      publish = {
-        enable = true;
-        userServices = true;
-      };
-      allowInterfaces = [ "br-lan" ];
-    };
+    extraConfig = ''
+      [Resolve]
+      MulticastDNS=yes
+    '';
   };
 }
