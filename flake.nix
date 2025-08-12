d5fe218c312e54494fce21f96cf3a5be99fbe887
ghostbuster91 <ghostbuster91@users.noreply.github.com>
2025-08-12 18:49:53 +0200

    Revert "Update nixpkgs and add deploy-rs"
    This reverts commit ea522ef2feb5e3ce1b027394c925e8932926a99a.

--- flake.nix
+++ flake.nix
@@ -28,11 +28,6 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
     deploy-rs.url = "github:serokell/deploy-rs";
-    devshell.url = "github:numtide/devshell";
-    agenix = {
-      url = "github:ryantm/agenix";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
   };
   outputs = inputs@{ flake-parts, ... }:
     flake-parts.lib.mkFlake { inherit inputs; } {
@@ -40,9 +35,7 @@
       imports = [
         ./modules
         ./machines
-        ./nix
         inputs.treefmt-nix.flakeModule
-        inputs.devshell.flakeModule
       ];
       perSystem.treefmt = {
         imports = [ ./treefmt.nix ];
