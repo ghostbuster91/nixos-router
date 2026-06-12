6ba33a804a0bd5cec0df151d4d45e4361a9cd5fd
ghostbuster91 <ghostbuster91@users.noreply.github.com>
2026-06-12 15:39:40 +0200

    Update garnix pub key

--- flake.nix
+++ flake.nix
@@ -1,7 +1,7 @@
 {
   nixConfig = {
     extra-substituters = [ "https://cache.garnix.io" ];
-    extra-trusted-public-keys = [ "cache.garnix.io:CTFPyKSLcx5RMi17nQsWZRzCfAY+fdv6OoF2u4xkqv8=" ];
+    extra-trusted-public-keys = [ "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g=" ];
   };
 
   inputs = {
